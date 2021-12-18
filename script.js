ae3c5ea3798934e1be03ec80e05d8958b78c9c6f
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-17 21:58:00 -0300

    Requisitos 9, 10 e 11: ok

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let corLista = document.querySelector('#lista-tarefas');
 corLista.addEventListener('click', mudaCor);
 
 function mudaCor(event) {
-  let pintarTarefa = document.querySelectorAll('.selected');
+  let pintarTarefa = document.querySelectorAll('.selected'); //classe esta sendo "pegada" do css
   let itemSelecionado = document.querySelector('.selected');
 
   if (pintarTarefa.length > 0) {
@@ -33,5 +33,35 @@ function mudaCor(event) {
     itemSelecionado.classList.add('selected');
   } else {
     event.target.classList.add('selected');
+  }
+}
+
+// Requisito 9
+let riscaLista = document.querySelector('#lista-tarefas');
+riscaLista.addEventListener('dblclick', risca);
+
+function risca(event) {
+  event.target.classList.toggle('completed'); //classe esta sendo "pegada" do css //.toggle remove e add o evento
+}
+
+//Requisito 10
+let deleteAll = document.querySelector('#apaga-tudo');
+deleteAll.addEventListener('click', apagarTudo);
+
+function apagarTudo() {
+  let apagar = document.querySelectorAll('li');
+  for (let i = 0; i < apagar.length; i += 1) {
+    apagar[i].remove();
+  }
+}
+
+//Requisito 11
+let deleteSelected = document.querySelector('#remover-finalizados');
+deleteSelected.addEventListener('click', apagarSelecionado);
+
+function apagarSelecionado() {
+  let itemSelecionado = document.querySelectorAll('.completed');
+  for (let i = 0; i < itemSelecionado.length; i += 1) {
+    itemSelecionado[i].parentElement.removeChild(itemSelecionado[i]);
   }
 }
