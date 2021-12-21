390841ee632c940dc4225c87fc60f601f756ebbb
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-21 18:06:27 -0300

    Requisito 14: ok

--- script.js
+++ script.js
@@ -1,67 +1,75 @@
-//Requisito 5 e 6
-let botao = document.querySelector('#criar-tarefa');
-botao.addEventListener('click', novoItem);
+window.onload = function () {
+  //Requisito 5 e 6
+  let botao = document.querySelector('#criar-tarefa');
+  botao.addEventListener('click', novoItem);
 
-function novoItem() {
-  let lista = document.querySelector('#lista-tarefas');
-  let tarefa = document.createElement('li');
-  tarefa.classList.add('tarefa');
-  let input = document.querySelector('#texto-tarefa').value;
-  let limpaInput = document.querySelector('#texto-tarefa');
+  function novoItem() {
+    let lista = document.querySelector('#lista-tarefas');
+    let tarefa = document.createElement('li');
+    tarefa.classList.add('tarefa');
+    let input = document.querySelector('#texto-tarefa').value;
+    let limpaInput = document.querySelector('#texto-tarefa');
 
-  if (input === '') {
-    alert('Erro: Digite nova tarefa!');
-  } else {
-    input.value = '';
-    tarefa.innerHTML = input;
-    lista.appendChild(tarefa);
+    if (input === '') {
+      alert('Erro: Digite nova tarefa!');
+    } else {
+      input.value = '';
+      tarefa.innerHTML = input;
+      lista.appendChild(tarefa);
+    }
+    limpaInput.value = '';
   }
-  limpaInput.value = '';
-}
 
-//Requisito 7 e 8 - com ajuda da Laís Nametala 19-A
-let corLista = document.querySelector('#lista-tarefas');
-corLista.addEventListener('click', mudaCor);
+  //Requisito 7 e 8 - com ajuda do Kleverson Eller 19-C
+  let corLista = document.querySelector('#lista-tarefas');
+  corLista.addEventListener('click', mudaCor);
 
-function mudaCor(event) {
-  let pintarTarefa = document.querySelectorAll('.selected'); //classe esta sendo "pegada" do css
-  let itemSelecionado = document.querySelector('.selected');
+  function mudaCor(event) {
+    let pintarTarefa = document.querySelectorAll('#lista-tarefas li');
 
-  if (pintarTarefa.length > 0) {
-    itemSelecionado.classList.remove('selected');
-    itemSelecionado = event.target;
-    itemSelecionado.classList.add('selected');
-  } else {
-    event.target.classList.add('selected');
+    for (let index of pintarTarefa) {
+      index.removeAttribute('id');
+      event.target.id = 'selected';
+    }
   }
-}
 
-// Requisito 9
-let riscaLista = document.querySelector('#lista-tarefas');
-riscaLista.addEventListener('dblclick', risca);
+  // Requisito 9
+  let riscaLista = document.querySelector('#lista-tarefas');
+  riscaLista.addEventListener('dblclick', risca);
 
-function risca(event) {
-  event.target.classList.toggle('completed'); //classe esta sendo "pegada" do css //.toggle remove e add o evento
-}
-
-//Requisito 10
-let deleteAll = document.querySelector('#apaga-tudo');
-deleteAll.addEventListener('click', apagarTudo);
-
-function apagarTudo() {
-  let apagar = document.querySelectorAll('li');
-  for (let i = 0; i < apagar.length; i += 1) {
-    apagar[i].remove();
+  function risca(event) {
+    event.target.classList.toggle('completed'); //classe esta sendo "pegada" do css //.toggle remove e add o evento
   }
-}
 
-//Requisito 11
-let deleteSelected = document.querySelector('#remover-finalizados');
-deleteSelected.addEventListener('click', apagarSelecionado);
+  //Requisito 10
+  let deleteAll = document.querySelector('#apaga-tudo');
+  deleteAll.addEventListener('click', apagarTudo);
 
-function apagarSelecionado() {
-  let itemSelecionado = document.querySelectorAll('.completed');
-  for (let i = 0; i < itemSelecionado.length; i += 1) {
-    itemSelecionado[i].parentElement.removeChild(itemSelecionado[i]);
+  function apagarTudo() {
+    let apagar = document.querySelectorAll('li');
+    for (let i = 0; i < apagar.length; i += 1) {
+      apagar[i].remove();
+    }
   }
-}
+
+  //Requisito 11
+  let deleteFinalizado = document.querySelector('#remover-finalizados');
+  deleteFinalizado.addEventListener('click', apagarFinalizado);
+
+  function apagarFinalizado() {
+    let itemSelecionado = document.querySelectorAll('.completed');
+    for (let i = 0; i < itemSelecionado.length; i += 1) {
+      itemSelecionado[i].parentElement.removeChild(itemSelecionado[i]);
+    }
+  }
+
+  //Requisito 14
+  let deleteSelecionado = document.querySelector('#remover-selecionado');
+  deleteSelecionado.addEventListener('click', apagarSelecionado);
+
+  function apagarSelecionado() {
+    let apagar = document.querySelector('#selected');
+    let item = document.querySelector('#lista-tarefas');
+    item.removeChild(apagar);
+  }
+};
