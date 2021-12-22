aea8f7fac71245aa64485f2a3c5c0f48ea5304e4
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-21 22:32:28 -0300

    Requisito 12: ok

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ window.onload = function () {
   function novoItem() {
     let lista = document.querySelector('#lista-tarefas');
     let tarefa = document.createElement('li');
-    tarefa.classList.add('tarefa');
     let input = document.querySelector('#texto-tarefa').value;
     let limpaInput = document.querySelector('#texto-tarefa');
 
@@ -63,7 +62,52 @@ window.onload = function () {
     }
   }
 
-  //Requisito 14
+  //Requisito 12 - com ajuda do Kleverson Eller 19-C
+  //O método JSON.stringify() converte valores em javascript para uma String  JSON.
+  //O método JSON.parse() analisa uma string JSON, construindo o valor ou um objeto JavaScript descrito pela string.
+
+  //! Salva Lista:
+  let salvaLista = document.querySelector('#salvar-tarefas');
+  salvaLista.addEventListener('click', listaSalva);
+
+  function listaSalva() {
+    let lista = document.querySelectorAll('#lista-tarefas li');
+    let salva = [];
+    for (let i of lista) {
+      let classe = i.classList.value;
+      let texto = i.innerHTML;
+      salva.push({
+        classes: classe,
+        textos: texto,
+      });
+    }
+    localStorage.setItem('listaSalva', JSON.stringify(salva));
+  }
+
+  //! Retorno a lista Salva
+  let retornaListaSalva = localStorage.getItem('listaSalva');
+  let transformaObjeto = JSON.parse(retornaListaSalva);
+  let listaRetornada = document.querySelector('#lista-tarefas');
+
+  function novaLista() {
+    if (transformaObjeto !== null) {
+      for (let i of transformaObjeto) {
+        let novaLi = document.createElement('li');
+        let novaClasse = i.classes;
+        if (novaClasse !== '') {
+          novaLi.classList.add(novaClasse);
+        }
+        let novoTexto = i.textos;
+        novaLi.innerHTML = novoTexto;
+        listaRetornada.appendChild(novaLi);
+      }
+    }
+  }
+  novaLista();
+
+  //Requisito 13 - com ajuda do Kleverson Eller 19-C
+
+  //Requisito 14 - com ajuda do Kleverson Eller 19-C
   let deleteSelecionado = document.querySelector('#remover-selecionado');
   deleteSelecionado.addEventListener('click', apagarSelecionado);
 
